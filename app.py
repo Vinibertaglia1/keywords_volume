4d0b0b8a4fb92ebd0c4087b2c66f9a1b10421eb0
Vinibertaglia1 <91101011+Vinibertaglia1@users.noreply.github.com>
2022-09-05 10:43:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,51 +7,59 @@ import numpy as np
 df = pd.read_csv('googleVolumeSearch.csv')
 
 st.markdown('<h1 style = "border-radius: 50px; color: #FFFFFF;text-align:center;text-transform: uppercase; background:-webkit-linear-gradient(#1088ff,#0bd6d4);"> Keyword Analysis </h1>',unsafe_allow_html=True)
-country = df['country'].unique().tolist()
+country = df.loc[:, 'country'].unique().tolist()
 country.insert(0,"")
 countries = st.selectbox("Choose a country",country)
 if countries != '':
     df_filtrado = df[df['country'] == countries]
 
-    df_filtrado['absolute'] = df_filtrado['absolute'].fillna(0)
-    z_scores = stats.zscore(df_filtrado['absolute'])
-    df_filtrado['volume_zscore'] = z_scores
-    q20, q50, q70, q95 = np.percentile(df_filtrado['volume_zscore'],[20,50,70,95])
+    df_filtrado.loc[:, 'absolute'] = df_filtrado.loc[:, 'absolute'].fillna(0)
+    norm = (df_filtrado.absolute - df_filtrado.absolute.min()) / (df_filtrado.absolute.max() - df_filtrado.absolute.min())
+    df_filtrado.loc[:, 'volume_norm'] = norm
+    q30, q50, q80, q98 = np.percentile(df_filtrado['volume_norm'],[30,50,80,98])
     
-    def get_percentile_by_value(value, q20 = q20, q50=q50,q70=q70, q95=q95):
+    def get_percentile_by_value(value, q30 = q30, q50=q50,q80=q80, q98=q98):
         label = ''
-        if value < q20:
+        if value < q30:
             label = 'Very Low'
-        elif (value >= q20) and (value < q50):
+        elif (value >= q30) and (value < q50):
             label = 'Low'
-        elif (value >= q50) and (value < q70):
+        elif (value >= q50) and (value < q80):
             label = 'Medium'
-        elif (value >= q70) and (value < q95):
+        elif (value >= q80) and (value < q98):
             label = 'High'
         else:
             label = 'Very High'
         return label 
 
-    df_filtrado['volume_label'] = df_filtrado['volume_zscore'].apply(get_percentile_by_value)
-    textos =  df_filtrado['keyword'].unique().tolist()
+    df_filtrado.loc[:, 'volume_label'] = df_filtrado.loc[:, 'volume_norm'].apply(get_percentile_by_value)
+    textos =  df_filtrado.loc[:, 'keyword'].unique().tolist()
     textos.insert(0,'')
 
     feature = st.selectbox("Choose a Feature", ['','Keywords Database','Enter your Keyword'])
     
-    def get_percentage_of_total(value, df=df_filtrado):
-        max =  df['volume_zscore'].max()
-        calculate = (100 * value) / max
-        round_percentage = np.abs(round(calculate,2))
+    df_filtrado.loc[:, 'position'] = df_filtrado.loc[:, 'absolute'].rank()
+    df_filtrado['volume_posit'] = (df_filtrado.position - df_filtrado.position.min()) / (df_filtrado.position.max() - df_filtrado.position.min())
+    
+
+    def get_percentage_of_total(value):
+        calculate = (100 * value) 
+        round_percentage = round(calculate,2)
         
         return round_percentage
+
+   
+    df_filtrado.loc[:, 'position_percentage'] = df_filtrado['volume_posit'].apply(get_percentage_of_total)
     
-    df_filtrado['volume_percentage'] = df_filtrado['volume_zscore'].apply(get_percentage_of_total)
+    saida = st.selectbox('Choose and press enter to add keywords',textos)
     
     if feature == 'Keywords Database':
-        saida = st.selectbox('Choose and press enter to add keywords',textos)
+        #saida = st.selectbox('Choose and press enter to add keywords',textos)
         df_text = df_filtrado[df_filtrado['keyword'] == saida]
-        if df_text['absolute'].iloc[-1] == 0:
-            df_text['volume_percentage'] = 0
+        
+        #if df_text['absolute'].iloc[-1] == 0:
+            #df_text['position_percentage'] = 0
+            
         col1, col2, col3 = st.columns(3)
         if saida != "":
             with col1:
@@ -59,10 +67,10 @@ if countries != '':
                 volume_absolute = df_text['absolute'].iloc[-1]
                 st.markdown(f"<h3> {volume_absolute}</h3>",unsafe_allow_html=True)
             with col2:
-                st.markdown("<h1>  Volume Percentage </h1>", unsafe_allow_html=True)
-                volume_percentage = df_text['volume_percentage'].iloc[-1]
+                st.markdown("<h1>  Position Percentage </h1>", unsafe_allow_html=True)
+                position_percentage = df_text['position_percentage'].iloc[-1]
                 
-                st.markdown(f"<h3> {volume_percentage} % </h3>", unsafe_allow_html=True)
+                st.markdown(f"<h3> {position_percentage} % </h3>", unsafe_allow_html=True)
 
             with col3:
                 st.markdown("<h1> Search Volume </h1>", unsafe_allow_html=True)
